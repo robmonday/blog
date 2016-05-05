878bb3d9375733a7239919e7309e471646ffb972
Rob Monday <rob.monday@gmail.com>
2016-05-05 09:05:52 -0500

    add gallery

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
 class Handler(webapp2.RequestHandler):
         def write(self, *a, **kw):
                 self.response.out.write(*a, **kw)
@@ -32,23 +37,24 @@ class Handler(webapp2.RequestHandler):
                 return t.render(params)
 
         def render(self, template, **kw):
-                self.write(self.render_str(template, **kw))
+                self.write(self.render_str(template, **kw) )
 
 class MainPage(Handler):
 
     def render_front(self, title="", art="", error=""):
-        self.render("index.html", title=title, art=art, error=error)
-
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+        self.render("index.html", title=title, art=art, error=error, arts=arts)
 
     def get(self):
         self.render_front()
-
 
     def post(self):
         title = self.request.get("title")
         art = self.request.get("art")
         if title and art:
-            self.write("Thanks!")
+            a = Art(title=title, art=art)
+            a.put()
+            self.redirect("/")
         else:
             error = "Please enter both a title and some artwork!"
             self.render_front(title, art, error)
