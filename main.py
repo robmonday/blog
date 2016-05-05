6745b1872085338a1a5b60eedc20c3993d2a446f
Rob Monday <rob.monday@gmail.com>
2016-05-05 06:04:39 -0500

    Add post method to main handler with basic validation, Fix CSS links

--- main.py
+++ main.py
@@ -24,20 +24,27 @@ template_dir = os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class Handler(webapp2.RequestHandler):
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
+        def write(self, *a, **kw):
+                self.response.out.write(*a, **kw)
 
-	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
+        def render_str(self, template, **params):
+                t = jinja_env.get_template(template)
+                return t.render(params)
 
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
+        def render(self, template, **kw):
+                self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
     def get(self):
         self.render("index.html")
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage)
-], debug=True)
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+        if title and art:
+                self.write("Thanks!")
+        else:
+                error = "Please enter both a title and some artwork!"
+                self.render("index.html", title = title, art = art, error = error)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
